36215f6fe00dc15ecdfe184650b70f2be332517a
jlchen <snailxr@163.com>
2013-04-15 09:51:46 +0800

    for jiali hehe

--- admin/views.py
+++ admin/views.py
@@ -1,1 +1,30 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from admin.forms import UserForm
+from admin.forms import RoleForm
+from django.template import RequestContext
+from admin.models import User
+
+def user(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            userValue = form.cleaned_data
+            print userValue['name']
+            print userValue
+            user=User(name=userValue['name'],email=userValue['email'],description=userValue['description'])
+            user.save()
+    else:
+        form = UserForm()
+    return render_to_response('user.html', {'form': form},context_instance=RequestContext(request))
+
+
+def role(request):
+    if request.method == 'POST':
+        form = RoleForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RoleForm()
+    return render_to_response('user.html', {'form': form},context_instance=RequestContext(request))
+
--- localhostCMS/urls.py
+++ localhostCMS/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from admin.views import user
+from admin.views import role
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^adminself/', include(admin.site.urls)),
+     url(r'^user/$',user),
+     url(r'^role/$',role),
 )
